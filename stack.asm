7eb4a56cde1f580e8010a0c3e52ee521a89d02fe
KAZI MEHEDI <kazirabbikmhr@gmail.com>
2022-04-12 03:59:08 +0600

    add stack

---
+++ stack.asm
@@ -0,0 +1,19 @@
+.stack 100h
+.data
+.code
+
+main proc
+    
+     mov ax,9
+     push ax
+     
+     mov bx,5
+     push bx
+     
+     pop ax
+     pop bx
+    
+    
+    main endp
+end main
+
